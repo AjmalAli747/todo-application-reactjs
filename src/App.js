dba7bc236acc28166a6f88d881084634fa8cc137
AjmalAli747 <mdajmalali747@gmail.com>
2022-10-31 14:17:06 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit 1111<code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="todoApp">
+        <form action="">
+          <input type="text" placeholder='Enter Todo List' />
+          <button>ADD</button>
+        </form>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
